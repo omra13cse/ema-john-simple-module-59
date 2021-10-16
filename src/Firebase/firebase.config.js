20e493246e375d91d16ff7c34a24d72314e889b9
omra13cse <omraanika12@gmail.com>
2021-10-16 22:58:17 +0600

    firebase

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAcAiSvmr-LKXoWOZn3fzIjlQWTIRIoof8",
+    authDomain: "ema-john-simple-17e44.firebaseapp.com",
+    projectId: "ema-john-simple-17e44",
+    storageBucket: "ema-john-simple-17e44.appspot.com",
+    messagingSenderId: "843044391662",
+    appId: "1:843044391662:web:d9c445abd86868a58f41ff"
+  };
+  export default firebaseConfig;
